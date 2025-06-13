326256c1343d1da63e9a3a42f54573f2c709a940
Sujal Taneja <sujaltaneja10@gmail.com>
2025-06-13 09:21:56 +0000

    added security headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from 'next';
 
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "frame-ancestors 'self';",
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
